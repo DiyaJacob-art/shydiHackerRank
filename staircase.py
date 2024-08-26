097fcbce2e153d0af36cc8092abf9df7dca273c7
DiyaJacob-art <diyasj02@gmail.com>
2024-08-26 12:34:02 +0530

    Create staircase.py

---
+++ staircase.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+
+def staircase(n):
+    for i in range(1, n + 1):
+        spaces = " " * (n - i)
+        hashes = "#" * i
+        print(spaces + hashes)
